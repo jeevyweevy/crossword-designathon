6aa3c7ec24d34a4f5f51d8c7882e30bfeb06f225
Sanjeev Kamath <skkamath2@gmail.com>
2023-10-22 00:34:10 -0400

    Easy Completeish

---
+++ visuals.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+import game
+
+window = tk.Tk()
+
+def easy_call():
+    print("EASY")
+    game.openEasy()
+
+def medium_call():
+    print("medium")
+def hard_call():
+    print("hard")
+def show_menu():
+    def easy_inner():
+        menuWindow.destroy()
+        easy_call()
+
+    def medium_inner():
+        menuWindow.destroy()
+        medium_call()
+
+    def hard_inner():
+        menuWindow.destroy()
+        hard_call()
+
+    menuWindow = tk.Tk()
+    menuWindow.title("Menu Window")
+    menuWindow.geometry("750x750")
+
+    background_image = tk.PhotoImage(file= "menuScreen.png")
+    easy = tk.PhotoImage(file= "easy.png")
+    medium = tk.PhotoImage(file= "medium.png")
+    hard = tk.PhotoImage(file = "hard.png")
+
+    background_label = tk.Label(menuWindow, image=background_image)
+    background_label.place(relwidth=1, relheight=1)
+
+    easy_button = tk.Button(menuWindow, image=easy, width=190, height=150, command= easy_inner)
+    easy_button.place(x=150, y=250)
+
+    medium_button = tk.Button(menuWindow, image=medium, width=190, height=150, command=medium_inner)
+    medium_button.place(x=400, y = 250)
+
+    hard_button = tk.Button(menuWindow, image=hard, width=190, height=150, command= hard_inner)
+    hard_button.place(x=250, y= 400)
+    menuWindow.mainloop()
+
+def homeScreen():
+    def show_menu_1():
+        window.destroy()
+        show_menu()
+    # Create a tkinter window
+    window.geometry("750x750")
+    window.title("Home Screen")
+
+    background_image = tk.PhotoImage(file="homescreen_background.png")
+    continue_image = tk.PhotoImage(file="continue.png")
+
+    background_label = tk.Label(window, image=background_image)
+    background_label.place(relwidth=1, relheight=1)
+
+    continue_button = tk.Button(window, image=continue_image, width=270, height=80, command=show_menu_1)
+    continue_button.pack(side="bottom", pady=30)
+
+    window.mainloop()
--- wordsearch.py
+++ wordsearch.py
@@ -8,9 +8,9 @@ def initialize_screen():
         terms = file.readlines()
     terms = [term.upper().strip() for term in terms]
 
-    words = random.sample(terms, 6)
+    words = random.sample(terms, 1)
 
-    grid_size = 20
+    grid_size = 15
     grid = [['_' for _ in range(grid_size)] for _ in range(grid_size)]
 
     orientations = ['leftright', 'updown', 'diagonalup', 'diagonaldown']
@@ -76,6 +76,82 @@ def initialize_screen():
                 grid[x][y] = random.choice(string.ascii_uppercase)
 
     print_grid(grid_size, grid, words)
+    return grid
+
+def initialize_screen_easy():
+    with open("easy_dictionary", "r") as file:
+        terms = file.readlines()
+    terms = [term.upper().strip() for term in terms]
+
+    words = random.sample(terms, 6)
+
+    grid_size = 12
+    grid = [['_' for _ in range(grid_size)] for _ in range(grid_size)]
+
+    orientations = ['leftright', 'updown']
+
+    for word in words:
+        word_length = len(word)
+        placed = False
+
+        while not placed:
+            orientation = random.choice(orientations)
+            if orientation == 'leftright':
+                step_x = 1
+                step_y = 0
+            if orientation == 'updown':
+                step_x = 0
+                step_y = 1
+            if orientation == 'diagonalup':
+                step_x = 1
+                step_y = -1
+            if orientation == 'diagonaldown':
+                step_x = 1
+                step_y = 1
+
+            x_position = random.randint(0, grid_size - 1)
+            y_position = random.randint(0, grid_size - 1)
+
+            ending_x = x_position + (word_length * step_x)
+            ending_y = y_position + (word_length * step_y)
+
+            if ending_x < 0 or ending_x >= grid_size: continue
+            if ending_y < 0 or ending_y >= grid_size: continue
+
+            failed = False
+
+            for i in range(word_length):
+                character = word[i]
+
+                new_position_x = x_position + i * step_x
+                new_position_y = y_position + i * step_y
+
+                char_new_position = grid[new_position_x][new_position_y]
+                if char_new_position != '_':
+                    if char_new_position == character:
+                        continue
+                    else:
+                        failed = True
+                        break
+            if failed:
+                continue
+            else:
+                for i in range(word_length):
+                    character = word[i]
+
+                    new_position_x = x_position + i * step_x
+                    new_position_y = y_position + i * step_y
+
+                    grid[new_position_x][new_position_y] = character
+                placed = True
+
+    for x in range(grid_size):
+        for y in range(grid_size):
+            if (grid[x][y] == '_'):
+                grid[x][y] = random.choice(string.ascii_uppercase)
+
+    print_grid(grid_size, grid, words)
+    return grid, words
 
 def print_grid(grid_size, grid, words):
     for x in range(grid_size):
